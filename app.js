c22d88bfe1324c5a24b91671e55de4d08c74e721
milan <pperisic.milan@gmail.com>
2022-09-18 01:05:38 +0200

    Fixed players name

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let fighters = ['akuma', 'balrog', 'bison', 'blanka', 'cammy', 'chun-li', 'dhals
 // DOM 
 const playerOneEl = document.querySelector('.player-one');
 const playerTwoEl = document.querySelector('.player-two');
+const playerOneName = document.querySelector('.player-one .card-title');
+const playerTwoName = document.querySelector('.player-two .card-title');
 const playerOneBtnEl = document.querySelector('button.player-one');
 const playerTwoBtnEl = document.querySelector('button.player-two');
 const randomBtnEl = document.querySelector('.random-btn');
@@ -12,11 +14,13 @@ const randomBtnEl = document.querySelector('.random-btn');
 const firstPlayer = () => {
   let playerOne = Math.floor(Math.random() * fighters.length);
   playerOneEl.children[0].setAttribute("src", "img/" + fighters[playerOne] + ".png");
+  playerOneName.textContent = fighters[playerOne];
 }
 
 const secondPlayer = () => {
   let playerTwo = Math.floor(Math.random() * fighters.length);
   playerTwoEl.children[0].setAttribute("src", "img/" + fighters[playerTwo] + ".png");
+  playerTwoName.textContent = fighters[playerTwo];
 }
 
 // Choose buttons
