f5cd514794e802006765b0fe1ebf13c24e4b00ee
milan <pperisic.milan@gmail.com>
2022-09-18 00:05:30 +0200

    Added btn-s for picking player 1 and player 2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let fighters = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png', '14.png']
 
-const btnEl = document.querySelector('.btn');
+const randomBtnEl = document.querySelector('.random-btn');
 const playerOneEl = document.querySelector('.player-one');
 const playerTwoEl = document.querySelector('.player-two');
 
-btnEl.addEventListener('click', () => {
+randomBtnEl.addEventListener('click', () => {
   let playerOneSrc = Math.floor(Math.random() * fighters.length) + 1;
   playerOneEl.children[0].setAttribute("src", "img/" + playerOneSrc + ".png");
   let playerTwoSrc = Math.floor(Math.random() * fighters.length) + 1;
