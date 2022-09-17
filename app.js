7d153a8b30f65c64704295a37078b7a278e54d46
milan <pperisic.milan@gmail.com>
2022-09-17 23:28:18 +0200

    Add fighters links

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let fighters = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png', '14.png']
+
 const btnEl = document.querySelector('.btn');
 
 btnEl.addEventListener('click', () => {
