4a2b5582da15be4b2cd0bc11e3bf1bc065f78c25
milan <pperisic.milan@gmail.com>
2022-09-18 00:22:12 +0200

    Comments

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+// Fighters
 let fighters = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png', '14.png']
 
-
+// DOM 
 const playerOneEl = document.querySelector('.player-one');
 const playerTwoEl = document.querySelector('.player-two');
 const playerOneBtnEl = document.querySelector('button.player-one');
 const playerTwoBtnEl = document.querySelector('button.player-two');
 const randomBtnEl = document.querySelector('.random-btn');
 
+//Functions for taking src of images
 const firstPlayer = () => {
   let playerOneSrc = Math.floor(Math.random() * fighters.length) + 1;
   playerOneEl.children[0].setAttribute("src", "img/" + playerOneSrc + ".png");
@@ -17,8 +19,7 @@ const secondPlayer = () => {
   playerTwoEl.children[0].setAttribute("src", "img/" + playerTwoSrc + ".png");
 }
 
-
-
+// Choose buttons
 playerOneBtnEl.addEventListener('click', () => {
   firstPlayer();
 })
